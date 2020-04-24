82ad9f37cade8dce3138ba18b66e4bef61bf7096
Ziwei Cheng <ziweic@andrew.cmu.edu>
2020-04-24 13:08:54 -0400

    Initial Commit

---
+++ libgo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  libgo
+//
+//  Created by Ziwei Cheng on 4/24/20.
+//  Copyright © 2020 Ziwei Cheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
