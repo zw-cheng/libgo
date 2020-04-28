fc0d6204faa656c1e32ef1f5f0a0f819128ca4d0
Ziwei Cheng <ziweic@andrew.cmu.edu>
2020-04-28 15:07:02 -0400

    add close()

--- libgo/Channel.h
+++ libgo/Channel.h
@@ -1,5 +1,6 @@
 #include "concurrentqueue.h"
 #include <iostream>
+#include <string>
 using namespace std;
 using namespace moodycamel;
 
@@ -13,6 +14,7 @@ public:
     chan();
     chan(int bs);
     
+    // simply call the constructor
     chan<T> make(int bs);
 
     // put value x into channel
@@ -20,45 +22,70 @@ public:
     
     // get an x from channel
     T receive ();
+    
+    // a sender close the channel, meaning there is no more producers
+    void close ();
 
 private:
     // members
-    int buffer_size;\
+    int buffer_size;
+    bool closed = false;
     ConcurrentQueue<T> q;
 };
 
 
+// class channel_closed : public runtime_error{
+//     public:
+//     explicit channel_closed(string &s) : runtime_error(s) {}
+// };
+
+
+// constructors
 template <typename T>
 chan<T>::chan() : buffer_size(0){
     // this should be special - an unbuffered channel should block
     cout << "Unbuffered channel initialized" << endl;
 }
 
-
 template <typename T>
 chan<T>::chan(int bs) : buffer_size(bs){
     cout << "Buffered channel initialized" << endl;
 }
 
-
 template <typename T>
 chan<T> make(int bs = 0) {
-    return chan<T>(bs);
+    return bs == 0 ? chan<T>() : chan<T>(bs);
 };
 
 
+// channel operations
 template <typename T>
 void chan<T>::send(T x){
     q.enqueue(x);
-    cout << "send called, approximate q size: " << q.size_approx()<<endl;
+    // cout << "send called, approximate q size: " << q.size_approx()<<endl;
 }
 
 
 template <typename T>
 T chan<T>::receive(){
     T top;
-    q.try_dequeue(top);
+
+    // block and try
+    while(!q.try_dequeue(top)){
+        if(closed){
+            throw runtime_error("channel closed");
+        }
+    }
+    
     return top;
 }
+
+// channel operations
+template <typename T>
+void chan<T>::close(){
+    closed = true;
+    cout << "this channel is closed" <<endl;
+}
+
 
 #endif /* channel_h */
--- libgo/main.cpp
+++ libgo/main.cpp
@@ -3,36 +3,76 @@
 #include "goroutine.h"
 
 template <typename T>
-void sum(vector<int> nums, chan<T> &ch){
-    int res=0;
-    for(auto n : nums)
+void sum(vector<int> nums, chan<T> &ch)
+{
+    int res = 0;
+    for (auto n : nums)
         res += n;
     ch.send(res); // ch <- res
 }
 
-int main(){
+void example1()
+{
+    auto ch = make<int>(2); // my_chan := make(chan int)
+
+    vector<int> nums{1, 2, 3, 4};
+
+    // it should be called by adding go, but for now we call them sequencially
+    sum(vector<int>(nums.begin(), nums.begin() + nums.size() / 2), ch);
+    sum(vector<int>(nums.begin() + nums.size() / 2, nums.end()), ch);
+
+    int res1 = ch.receive();
+    int res2 = ch.receive();
+
+    cout << "sum" << res1 << " + sum" << res2 << " = result" << res1 + res2 << endl;
+}
+
+
+template <typename T>
+void fib(int n, chan<T> &ch)
+{
+    int x=0, y=1;
+    for (int i =0;i<n;i++){
+        ch.send(x);
+        int temp = x;
+        x = y;
+        y = temp + y;
+    }
+    ch.close();
+}
+
+void example2()
+{
+    auto ch = make<int>(10); // my_chan := make(chan int)
+
+    // it should be called by adding go, but for now we call them sequencially
+    fib(10, ch);
+
+    while(1){
+        int x;
+        try {
+            x = ch.receive();
+        } catch(std::exception const& e){
+            std::cout << "Exception: " << e.what() << endl;
+            break;
+        }
+        cout << x <<endl;
+    }
+}
+
+int main()
+{
     coroutine<void>::pull_type source{cooperative};
     cout << ", ";
     source();
     cout << "!\n";
-    
+
     // ------------- above is just some test code of boost::coroutine
-    
-    // todo: go sum()
-    
-    auto ch = make<int>(2); // my_chan := make(chan int)
-    
-    vector<int> nums{1,2,3,4};
-    
-    // it should be called by adding go, but for now we call them sequencially
-    sum(vector<int>(nums.begin(), nums.begin() + nums.size()/2), ch);
-    sum(vector<int>(nums.begin() + nums.size()/2, nums.end()), ch);
-    
-    int res1 = ch.receive();
-    int res2 = ch.receive();
-    
-    cout<< "sum" << res1 << " + sum" << res2 << " = result"<< res1+res2 <<endl;
-    
+    cout << "\nExample 1 :\n";
+    example1();
+
+    cout << "\nExample 2 :\n";
+    example2();
+
     return 0;
 }
-
