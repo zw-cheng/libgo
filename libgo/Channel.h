dcd985e32d90974836c9262d143bb4c4ad0b582d
Ziwei Cheng <ziweic@andrew.cmu.edu>
2020-05-03 00:34:48 -0400

    overloaded operator and naive buffered channe;

--- libgo/Channel.h
+++ libgo/Channel.h
@@ -12,43 +12,43 @@ class chan {
 public:
     // constructors
     chan();
+
     chan(int bs);
     
-    // simply call the constructor
-    chan<T> make(int bs);
+    // // simply call the constructor
+    // chan<T> make(int bs);
+
 
     // put value x into channel
     void send (T x);
     
     // get an x from channel
-    T receive ();
+    T receive (T &x);
     
     // a sender close the channel, meaning there is no more producers
     void close ();
 
+
+    void operator<<(T &x);
+    T operator>>(T &x);
+
 private:
-    // members
+    int current_size;
     int buffer_size;
     bool closed = false;
     ConcurrentQueue<T> q;
 };
 
 
-// class channel_closed : public runtime_error{
-//     public:
-//     explicit channel_closed(string &s) : runtime_error(s) {}
-// };
-
-
 // constructors
 template <typename T>
-chan<T>::chan() : buffer_size(0){
+chan<T>::chan() : buffer_size(INT_MAX), current_size(0){
     // this should be special - an unbuffered channel should block
     cout << "Unbuffered channel initialized" << endl;
 }
 
 template <typename T>
-chan<T>::chan(int bs) : buffer_size(bs){
+chan<T>::chan(int bs) : buffer_size(bs), current_size(0){
     cout << "Buffered channel initialized" << endl;
 }
 
@@ -61,23 +61,23 @@ chan<T> make(int bs = 0) {
 // channel operations
 template <typename T>
 void chan<T>::send(T x){
+    while(current_size >= buffer_size);
+    current_size++;
     q.enqueue(x);
     // cout << "send called, approximate q size: " << q.size_approx()<<endl;
 }
 
 
 template <typename T>
-T chan<T>::receive(){
-    T top;
-
+T chan<T>::receive(T &x){
     // block and try
-    while(!q.try_dequeue(top)){
+    while(!q.try_dequeue(x)){
         if(closed){
             throw runtime_error("channel closed");
         }
     }
-    
-    return top;
+    current_size--;
+    return x;
 }
 
 // channel operations
@@ -87,5 +87,15 @@ void chan<T>::close(){
     cout << "this channel is closed" <<endl;
 }
 
+template<typename T>
+void chan<T>::operator<<(T &x){
+    return this->send(x);
+}
+
+template<typename T>
+T chan<T>::operator>>(T &x){
+    // todo : Exception
+    return this->receive(x);
+}
 
 #endif /* channel_h */
--- libgo/main.cpp
+++ libgo/main.cpp
@@ -8,12 +8,12 @@ void sum(vector<int> nums, chan<T> &ch)
     int res = 0;
     for (auto n : nums)
         res += n;
-    ch.send(res); // ch <- res
+    ch << res; // ch <- res
 }
 
 void example1()
 {
-    auto ch = make<int>(2); // my_chan := make(chan int)
+    auto ch = make<int>(0); // my_chan := make(chan int)
 
     vector<int> nums{1, 2, 3, 4};
 
@@ -21,18 +21,20 @@ void example1()
     sum(vector<int>(nums.begin(), nums.begin() + nums.size() / 2), ch);
     sum(vector<int>(nums.begin() + nums.size() / 2, nums.end()), ch);
 
-    int res1 = ch.receive();
-    int res2 = ch.receive();
+    int res1;
+    ch >> res1;
+    int res2;
+    ch >> res2;
 
     cout << "sum" << res1 << " + sum" << res2 << " = result" << res1 + res2 << endl;
 }
 
-
 template <typename T>
 void fib(int n, chan<T> &ch)
 {
-    int x=0, y=1;
-    for (int i =0;i<n;i++){
+    int x = 0, y = 1;
+    for (int i = 0; i < n; i++)
+    {
         ch.send(x);
         int temp = x;
         x = y;
@@ -43,20 +45,24 @@ void fib(int n, chan<T> &ch)
 
 void example2()
 {
-    auto ch = make<int>(10); // my_chan := make(chan int)
+    auto ch = chan<int>(10); // my_chan := make(chan int)
 
     // it should be called by adding go, but for now we call them sequencially
     fib(10, ch);
 
-    while(1){
+    while (1)
+    {
         int x;
-        try {
-            x = ch.receive();
-        } catch(exception const& e){
+        try
+        {
+            ch.receive(x);
+        }
+        catch (exception const &e)
+        {
             cout << "Exception: " << e.what() << endl;
             break;
         }
-        cout << x <<endl;
+        cout << x << endl;
     }
 }
 
@@ -68,7 +74,7 @@ int main()
     cout << "!\n";
 
     // ------------- above is just some test code of boost::coroutine
-    
+
     cout << "\nExample 1 :\n";
     example1();
 
